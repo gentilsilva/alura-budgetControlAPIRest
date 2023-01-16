4cf4a0b36b298991639d403fb1516fb654b335fc
Gentil Silva <silvagentil.sg@gmail.com>
2023-01-16 05:19:05 -0300

    Criando as entidades Receita e Despesa

---
+++ api/src/test/java/budget/control/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package budget.control.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
